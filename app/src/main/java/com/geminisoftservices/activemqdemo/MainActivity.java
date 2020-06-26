1ff8468250f114be5181139f21984064c7fe55bf
Ayush Rastogi <ayush.rastogi@daalchini.co.in>
2020-06-26 14:02:53 +0530

    Update -- Code for connection, subscribe and publish message.

--- app/src/main/java/com/geminisoftservices/activemqdemo/MainActivity.java
+++ app/src/main/java/com/geminisoftservices/activemqdemo/MainActivity.java
@@ -3,12 +3,89 @@ package com.geminisoftservices.activemqdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String serverURI = "tcp://192.168.43.72:1883";
+    private final String clientId = "Ayush";
+    private MqttAndroidClient client;
+    private Button btnPublish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnPublish = findViewById(R.id.publish);
+        connect();
+
+        btnPublish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                publishMessage("Hi There, this is a test publish message from android client");
+            }
+        });
+    }
+
+    private void connect() {
+        MqttConnectOptions connectOptions = new MqttConnectOptions();
+        connectOptions.setAutomaticReconnect(true);
+
+        client = new MqttAndroidClient(this, serverURI, clientId);
+        try {
+            client.connect(connectOptions, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    subscribe();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void subscribe() {
+        String subscribeTopic = "sub_topic";
+        try {
+            client.subscribe(subscribeTopic, 0, new IMqttMessageListener() {
+                @Override
+                public void messageArrived(final String topic, final MqttMessage message) throws Exception {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, message.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void publishMessage(String message) {
+        String publishTopic = "pub_topic";
+        MqttMessage msg = new MqttMessage();
+        msg.setPayload(message.getBytes());
+        try {
+            client.publish(publishTopic, msg);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 }
